f0d06a212e1f4bafb4e259314a18ef9f3e5e7640
Brandon Reninger <brandonreninger@yahoo.com>
2020-03-25 16:35:14 -0600

    couple steps from showing houses

--- latespring2020-gregslist-mvc-master/app/Models/House.js
+++ latespring2020-gregslist-mvc-master/app/Models/House.js
@@ -7,4 +7,19 @@ export default class House {
         this.squarefeet = data.squarefeet
         this.imgUrl = data.imgUrl
     }
+
+    getHouseTemplate() {
+        return /*html*/ `
+        <div class="col-4 border border-info rounded shadow">
+            <h3>Address: ${this.address}</h3>
+            <h5>Price: ${this.price}</h5>
+            <h5>Bedrooms: ${this.bedrooms}</h5>
+            <h5>Bathrooms: ${this.bathrooms}</h5>
+            <h5>Square Ft: ${this.squarefeet}</h5>
+            <img class="img-fluid" src="${this.imgUrl}">
+            <button class = "btn btn-danger btn-block"
+            onclick = "app.houseController.delete(${index})">Delete</button>
+        </div>
+        `
+    }
 }
